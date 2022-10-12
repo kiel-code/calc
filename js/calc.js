7dd38513127ab268cbdeb832459981cd77b1c2db
Atanda Ezekiel <ezekielaa3@outlook.com>
2022-10-12 06:51:01 +0100

    JS add on

--- js/calc.js
+++ js/calc.js
@@ -1,79 +1,80 @@
 let numeric = '';
-let final = []
+let final = [];
 let symbolic = [];
 
-document.querySelector('#btn').addEventListener('click', (press) => {
-  switch (press.target.innerText) {
+document.querySelector('#btn').addEventListener('click', (e) => {
+  switch (e.target.innerText) {
     case '1':
-      document.getElementById('display').innerText += '1'
-      numeric += '1'
+      document.getElementById('display').innerText += '1';
+      numeric += '1';
       break;
     case '2':
-      document.getElementById('display').innerText += '2'
-      numeric += '2'
+      document.getElementById('display').innerText += '2';
+      numeric += '2';
       break;
     case '3':
-      document.getElementById('display').innerText += '3'
-      numeric += '3'
+      document.getElementById('display').innerText += '3';
+      numeric += '3';
       break;
     case '4':
-      document.getElementById('display').innerText += '4'
-      numeric += '4'
+      document.getElementById('display').innerText += '4';
+      numeric += '4';
       break;
     case '5':
-      document.getElementById('display').innerText += '5'
-      numeric += '5'
+      document.getElementById('display').innerText += '5';
+      numeric += '5';
       break;
     case '6':
-      document.getElementById('display').innerText += '6'
-      numeric += '6'
+      document.getElementById('display').innerText += '6';
+      numeric += '6';
       break;
     case '7':
-      document.getElementById('display').innerText += '7'
-      numeric += '7'
+      document.getElementById('display').innerText += '7';
+      numeric += '7';
       break;
     case '8':
-      document.getElementById('display').innerText += '8'
-      numeric += '8'
+      document.getElementById('display').innerText += '8';
+      numeric += '8';
       break;
     case '9':
-      document.getElementById('display').innerText += '9'
-      numeric += '9'
+      document.getElementById('display').innerText += '9';
+      numeric += '9';
+      break;
+      case '0':
+      document.getElementById('display').innerText += '0';
+      numeric += '0';
       break;
     case '=':
-      document.getElementById('display').innerText += '='
+      document.getElementById('display').innerText += '=';
       final.push(parseInt(numeric));
       numeric = '';
+      // result.final
       break;
     case '+':
-      document.getElementById('display').innerText += '+'
-      symbolic.push('+')
+      document.getElementById('display').innerText += '+';
+      symbolic.push('+');
       final.push(parseInt(numeric));
       numeric = '';
       break;
     case '-':
-      document.getElementById('display').innerText += '-'
-      symbolic.push('-')
+      document.getElementById('display').innerText += '-';
+      symbolic.push('-');
       final.push(parseInt(numeric));
       numeric = '';
       break;
     case '*':
-      document.getElementById('display').innerText += '*'
-      symbolic.push('*')
+      document.getElementById('display').innerText += '*';
+      symbolic.push('*');
       final.push(parseInt(numeric));
       numeric = '';
       break;
     case '/':
-      document.getElementById('display').innerText += '/'
-      symbolic.push('/')
+      document.getElementById('display').innerText += '/';
+      symbolic.push('/');
       final.push(parseInt(numeric));
       numeric = '';
       break;
   }
-  // let display = document.getElementById('display').innerText;
-  // console.log(display);
-  // const result = eval(display);
-  // console.log(result);
   
   let result = final[0];
 
@@ -88,7 +89,13 @@ document.querySelector('#btn').addEventListener('click', (press) => {
       } else if (symbolic[z - 1] === '/') {
         result /= final[z];
       }
+      
     }
   }
   console.log(result)
 })
+
+// let x = 5;
+// let y = 5;
+// let q = x * y;
+// console.log(q)
